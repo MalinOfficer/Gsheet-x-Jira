1b5f50810dc0fbf4c6eba5a6a1dac0119d3f1fab
Muh Malindo <muh.malindo.fn@gmail.com>
2025-08-07 16:01:08 +0000

    Pada halaman Update Case L3 tambahkan function dimana user dapat me link

--- src/app/update-case-l3/page.tsx
+++ src/app/update-case-l3/page.tsx
@@ -1,10 +1,7 @@
+import { GsheetDashboard } from "@/components/gsheet-dashboard";
+
 export default function UpdateCaseL3Page() {
     return (
-        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-            <div className="grid gap-4">
-                <h1 className="text-2xl font-bold tracking-tight">Update Case L3</h1>
-                <p className="text-muted-foreground">This page is under construction.</p>
-            </div>
-        </main>
+        <GsheetDashboard />
     );
 }
