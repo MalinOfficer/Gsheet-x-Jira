40c433d09c6bb3dac7b41a954ebe31d255d63c8d
Malin Officer <malinofficer@gmail.com>
2025-09-16 07:12:13 +0000

    kenapa masih seperti ini?

--- next.config.ts
+++ next.config.ts
@@ -8,17 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
