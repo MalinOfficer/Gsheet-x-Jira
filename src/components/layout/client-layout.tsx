fba1fdc4aa33eaaf8136b1ba92d16dd0b03a7cf9
Muh Malindo <muh.malindo.fn@gmail.com>
2025-09-30 06:19:32 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -43,7 +43,7 @@ function NavLinksDesktop() {
         <NavigationMenuList>
           {primaryNavItems.map((item) => (
             <NavigationMenuItem key={item.label}>
-              <Link href={item.href} legacyBehavior passHref>
+              <Link href={item.href} passHref>
                 <NavigationMenuLink
                   active={pathname === item.href}
                   className={navigationMenuTriggerStyle()}
@@ -218,27 +218,27 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a"> & { icon?: React.ElementType }
->(({ className, title, children, icon: Icon, href, ...props }, ref) => {
+>(({ className, title, children, icon: Icon, href = "#", ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link href={href || "#"} legacyBehavior passHref>
-            <a
+        <Link href={href} legacyBehavior passHref>
+          <a
             ref={ref}
             className={cn(
-                "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                className
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className
             )}
             {...props}
-            >
-                <div className="flex items-center gap-2">
-                    {Icon && <Icon className="h-5 w-5" />}
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                </div>
-                <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                    {children}
-                </p>
-            </a>
+          >
+            <div className="flex items-center gap-2">
+              {Icon && <Icon className="h-5 w-5" />}
+              <div className="text-sm font-medium leading-none">{title}</div>
+            </div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
         </Link>
       </NavigationMenuLink>
     </li>
@@ -246,5 +246,7 @@ const ListItem = React.forwardRef<
 })
 ListItem.displayName = "ListItem"
 
+
+    
 
     
