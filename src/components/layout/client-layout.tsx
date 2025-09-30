ed91bf9b3e3a4b1d82eafab3ba3156b61dfa80b6
Muh Malindo <muh.malindo.fn@gmail.com>
2025-09-30 05:08:19 +0000

    Masih terjadi perbaiki ini sampai tuntas

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -43,7 +43,7 @@ function NavLinksDesktop() {
         <NavigationMenuList>
           {primaryNavItems.map((item) => (
             <NavigationMenuItem key={item.label}>
-                <Link href={item.href} legacyBehavior passHref>
+                <Link href={item.href} passHref>
                     <NavigationMenuLink active={pathname === item.href} className={navigationMenuTriggerStyle()}>
                         <item.icon className="h-4 w-4 mr-2 shrink-0" />
                         {item.label}
@@ -218,7 +218,7 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -233,7 +233,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   )
