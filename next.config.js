25b452f1da61724ba54948348ef8d6f18227839a
PINTRO's <pintros@PINTROss-MacBook-Pro.local>
2025-12-09 14:45:14 +0700

    csvDuplikatHeder

---
+++ next.config.js
@@ -0,0 +1,19 @@
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true, // Ini adalah bagian penting, memaksa service worker baru untuk segera aktif
+  disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
