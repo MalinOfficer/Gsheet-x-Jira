5611e2a5b3e044f48d8f153f936d81cf695995c4
Malin Officer <malinofficer@gmail.com>
2025-12-09 09:11:03 +0000

    module not found?

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -20,8 +20,8 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import React from "react";
-import { ThemeSwitch } from "@/components/ui/theme-switch";
-import { Spinner } from "@/components/ui/spinner";
+import { ThemeSwitch } from "../ui/theme-switch";
+import { Spinner } from "../ui/spinner";
 
 
 const primaryNavItems = [
