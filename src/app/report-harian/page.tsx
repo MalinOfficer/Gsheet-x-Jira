2759439d094b58a0be9003638f71bc10c4e9d89b
Malin Officer <malinofficer@gmail.com>
2026-01-29 04:35:46 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/report-harian/page.tsx
+++ src/app/report-harian/page.tsx
@@ -5,7 +5,7 @@ import dynamic from 'next/dynamic';
 import { Skeleton } from '@/components/ui/skeleton';
 
 const ReportHarian = dynamic(
-    () => import('../../components/report-harian').then(mod => mod.ReportHarian),
+    () => import('@/components/report-harian').then(mod => mod.ReportHarian),
     { 
         ssr: false,
         loading: () => (
